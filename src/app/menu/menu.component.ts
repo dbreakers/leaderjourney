1270741f21f1fcc78225debe3365c9d105340805
dbreakers <david.breakwell@gmail.com>
2020-07-20 12:53:29 +0100

    2 files modified

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -12,7 +12,10 @@ import { TrainingComponent } from '../training/training.component';
 export class MenuComponent implements OnInit {
   selected_role = {};
   plp = [];
-  
+  fa = "";
+  sf = "";
+  sg = "";
+  gd = "";
   
   constructor(private navi: OnsNavigator,
               private globals: Globals,) {
@@ -24,10 +27,43 @@ export class MenuComponent implements OnInit {
     this.navi.nativeElement.pushPage(TrainingComponent);
   }
   
+ formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+  find_mandatory (mantype) {
+    var expiry="1900-01-01"
+    for(var i=0;i<this.globals.compassdata.object.mandate.length; i++){
+      var item = this.globals.compassdata.object.mandate[i];
+      if(item.expiry>expiry&&item.mandCode==mantype) {
+         expiry = item.expiry;
+      }
+    } 
+    return expiry;
+  }
+
+  get_mandatory(){
+  var date = this.formatDate("")
+  var expiry = this.find_mandatory("FA");
+  if (expiry < date) {
+    this.fa = "od";
+  }   
+  }
+  
   ngOnInit() {
 this.selected_role = this.globals.compassdata.object.roles.find(r=>r.id == this.globals.roleid )
 this.plp = this.globals.compassdata.object.plps[this.globals.roleid];
-
+this.get_mandatory() 
  
   }
 }
