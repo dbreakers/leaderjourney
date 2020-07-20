1a18be5c241d1dfd757c592c75174e9680e4a4de
dbreakers <david.breakwell@gmail.com>
2020-07-20 19:06:48 +0100

    3 files modified

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,9 +10,20 @@ import { Globals } from '../globals';
 })
 export class CardComponent implements OnInit {
 yellowcard='[{"type": "mh","text": "Young people first"},{"type": "sh","text": "Safeguarding – a code of practice"},{"type": "tx","text": "This card contains essential information for all adults in Scouting. Please keep it with you at all times."},{"type": "tx", "text":"This card contains essential information for all adults in Scouting. Please keep it with you at all times."},{"type": "tx", "text":"Version 7. Nov 2018"},{"type": "bx", "text":"It is the policy of the Scouts to safeguard the welfare of all young people by protecting them from neglect and from physical, sexual and emotional harm. All members have a duty to report concerns or suspicions and a right to do so in confidence and free from harassment"}]'
-yellowcardl = {}
+greencard='[{"type": "mh","text": "Alcohol"},{"type": "sh","text": "Safeguarding – a code of practice"},{"type": "tx","text": "This card contains essential information for all adults in Scouting. Please keep it with you at all times."},{"type": "tx", "text":"This card contains essential information for all adults in Scouting. Please keep it with you at all times."},{"type": "tx", "text":"Version 7. Nov 2018"},{"type": "bx", "text":"It is the policy of the Scouts to safeguard the welfare of all young people by protecting them from neglect and from physical, sexual and emotional harm. All members have a duty to report concerns or suspicions and a right to do so in confidence and free from harassment"}]'
+cardl = {}
+color="y"
 
+do_alcohol() {
+  this.cardl = JSON.parse(this.greencard);
+  this.color = "g"
+}
+do_sg() {
+  this.cardl = JSON.parse(this.yellowcard);
+  this.color = "y"
+}
   ngOnInit() {
-   this.yellowcardl = JSON.parse(this.yellowcard);
+   this.cardl = JSON.parse(this.yellowcard);
+   this.color = "y"
   }
 } 
