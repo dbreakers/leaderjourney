ccd7e17a7b632545a50fe7fd7fd3442994affff2
dbreakers <david.breakwell@gmail.com>
2020-07-23 19:26:51 +0100

    2 files modified

--- src/app/test.js
+++ src/app/test.js
@@ -1,36 +1,41 @@
-exports.screenshot = functions
-  .runWith({
-    timeoutSeconds: 120,
-    memory: "2GB"
-  })
-  .https.onRequest((request, response) => {
-    (async () => {
-      const url = request.query.url;
-      const width = parseInt(request.query.width) || 1024;
-      const height = parseInt(request.query.height) || 768;
-      const fullPage = request.query.full
-        ? request.query.full === "true"
-        : false;
-
-      if (!url) {
-        return response.send(`Invalid url: ${url}`);
-      }
-
-      const browser = await puppeteer.launch({
-        args: ["--no-sandbox"]
-      });
-      const page = await browser.newPage();
-
-      await page.goto(url, { waitUntil: "networkidle2" });
-
-      if (!fullPage) {
-        await page.setViewport({ width, height });
-      }
-
-      const screenshot = await page.screenshot({ fullPage });
-
-      await browser.close();
-
-      return response.type("image/png").send(screenshot);
-    })();
+const express = require('express');
+const functions = require('firebase-functions');
+const puppeteer = require('puppeteer');
+const app = express();
+// Runs before every route. Launches headless Chrome.
+app.all('*', async (req, res, next) => {
+  // Note: --no-sandbox is required in this env.
+  // Could also launch chrome and reuse the instance
+  // using puppeteer.connect()
+  res.locals.browser = await puppeteer.launch({
+    args: ['--no-sandbox']
   });
+  next(); // pass control to next route.
+});
+// Handler to take screenshots of a URL.
+app.get('/screenshot', async function screenshotHandler(req, res) {
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).send(
+      'Please provide a URL. Example: ?url=https://example.com');
+  }
+  const browser = res.locals.browser;
+  try {
+    const page = await browser.newPage();
+    await page.goto(url, {waitUntil: 'networkidle2'});
+    const buffer = await page.screenshot({fullPage: true});
+    res.type('image/png').send(buffer);
+  } catch (e) {
+    res.status(500).send(e.toString());
+  }
+  await browser.close();
+});
+// Handler that prints the version of headless Chrome being used.
+app.get('/version', async function versionHandler(req, res) {
+  const browser = res.locals.browser;
+  res.status(200).send(await browser.version());
+  await browser.close();
+});
+const opts = {memory: '2GB', timeoutSeconds: 60};
+exports.screenshot = functions.runWith(opts).https.onRequest(app);
+exports.version = functions.https.onRequest(app);
