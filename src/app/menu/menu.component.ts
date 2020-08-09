f3ee66fc1d4a2052b36761d60f730a58b706c453
dbreakers <david.breakwell@gmail.com>
2020-08-09 15:57:45 +0100

    1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -134,7 +134,7 @@ export class MenuComponent implements OnInit {
   }
 
   get_role() {
-    this.selected_role = this.globals.compassar[0].find(r=>r.roleid == this.globals.roleid )
+    returnthis.selected_role = this.globals.compassar[0].find(r=>r.roleid == this.globals.roleid )
   }
   
   ngOnInit() {
