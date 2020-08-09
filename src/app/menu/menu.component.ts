8910b0d15f3107b524208dd39d32d797d323be9c
dbreakers <david.breakwell@gmail.com>
2020-08-09 16:37:21 +0100

    1 file modified

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -138,11 +138,15 @@ export class MenuComponent implements OnInit {
   get_role() {
     return this.selected_role = this.globals.compassar[0].find(r=>r.roleid == this.globals.roleid )
   }
+
+  get_user(user){ 
+    this.globals.compassuser  = user;
+  }
   
   ngOnInit() {
 this.selected_role = this.globals.compassar[0].find(r=>r.roleid == this.globals.roleid )
 if (this.globals.compassuser.length==0){
-   this.callnetworkService.getUser().subscribe(ar=> this.get_all(ar));
+   this.callnetworkService.getUser().subscribe(user=> this.get_user(user));
 }
 //this.plp = this.globals.compassdata.object.plps[this.globals.roleid];
 //this.get_mandatory() 
