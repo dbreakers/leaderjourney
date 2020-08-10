afe01c0d755cfe998a5b9454ef3d4d806db3d7f8
dbreakers <david.breakwell@gmail.com>
2020-08-10 19:03:47 +0100

    1 file modified

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -23,7 +23,14 @@ export class TrainingComponent implements OnInit {
               private _params: Params,
               private globals: Globals,) {
   } 
- 
+
+  compass_date(longdate) {
+  if (longdate.split(" ").length<3) { return ""}
+  return longdate.split(" ")[2]+"-"+
+                    (this.globals.months.indexOf( longdate.split(" ")[1])+1)
+                    + "-" +longdate.split(" ")[0];
+} 
+
   mandatory_find() {
   this.count = 0;  
   for (var i=0; i<this.plp.length; i++) {
@@ -33,11 +40,10 @@ export class TrainingComponent implements OnInit {
   this.width = Math.floor( 90 * this.count / this.plp.length);
   this.status = "ok";
   var nowsum = 0
-  //var utc = new Date(this.selected_role.datefrom).toUTCString();
-/*
-   this.targetyear = parseInt(this.selected_role.datefrom.substring(0,4));
+  var utc = new Date(this.compass_date(this.selected_role.start)).toUTCString();
+  this.targetyear = parseInt(this.compass_date(this.selected_role.start).substring(0,4));
    this.targetyear = this.targetyear + 3; 
-  this.targetmonth = this.monthlist[parseInt(this.selected_role.datefrom.substring(5,7))-1];  
+  this.targetmonth = this.globals.months[parseInt(this.compass_date(this.selected_role.start).substring(5,7))-1];  
   this.status = "ok";
   if ((this.targetyear<(new Date()).getFullYear())){
     this.status = "od";
@@ -48,7 +54,7 @@ export class TrainingComponent implements OnInit {
     this.status = "od";
   }
   var nowsum = (new Date()).getFullYear() * 12 + (new Date()).getMonth();
-  var targetsum = this.targetyear * 12 + parseInt(this.selected_role.datefrom.substring(5,7))-1;*/
+  var targetsum = this.targetyear * 12 + parseInt(this.selected_role.datefrom.substring(5,7))-1;
   if ((nowsum-targetsum)<4) {this.status="du"}
   if (this.percent==100) {this.status="ok"} 
    }
