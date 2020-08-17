b082fa358027c977feeb6181fd8f4cdac76173ac
dbreakers <david.breakwell@gmail.com>
2020-08-17 19:48:51 +0100

    2 files modified

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OnsNavigator,  Params, } from 'ngx-onsenui';
 import { Globals } from '../globals';
- 
+import { AddressCardComponent } from '../addresscard/addresscard.component'; 
 
 @Component({
   selector: 'ons-page[directory]',
@@ -27,6 +27,14 @@ var finditem = true;
   }
 return finditem
 }  
+
+goto_card2(u) {
+this.navi.nativeElement.pushPage(AddressCardComponent,{data: {data: u[0] , roleid: this.globals.compassuser[0].roles[0].roleid}});
+}
+
+goto_card(id) {
+this.callnetworkService.getRoleUserAddress(this.globals.roleid,id).subscribe(user=> this.goto_card2(user));
+}
 
 update_search($event) {
   for (var i = 0; i < this.globals.last_directory[0].length; i++) {
