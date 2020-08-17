8a5fcc589879e1e57b33633392240006f5e6aa63
dbreakers <david.breakwell@gmail.com>
2020-08-17 17:49:46 +0100

    3 files modified

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -10,12 +10,43 @@ import { Globals } from '../globals';
 })
 export class DirectoryComponent implements OnInit {
 
+namefilter = "";
+
   constructor(private navi: OnsNavigator,
               private _params: Params,
               private globals: Globals,) {
   } 
 
-  
+searchObj(member) {
+var search_strings = this.namefilter.split(" ");
+var finditem = true;
+  for (var i=0; i<search_strings.length;i++){
+    if (member.name.indexOf(search_strings[i])==-1) {
+     finditem = false;
+    }
+  }
+return finditem
+}  
+
+update_search($event) {
+  for (var i = 0; i < this.globals.last_directory[0].length; i++) {
+      var v = document.getElementById(this.globals.last_directory[0][i].contact_number);
+
+      if (this.searchObj(this.globals.last_directory[0][i])) {
+       
+         v.classList.add("show"); 
+        v.classList.remove("hidden");
+        
+      } else
+      {
+        v.classList.remove("show"); 
+        v.classList.add("hidden");
+      }
+        
+      
+    }
+ 
+  }
   
 ngOnInit() {
   
