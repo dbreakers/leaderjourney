c8abb10584524d71027302af11fb14d79f8f7f5f
dbreakers <david.breakwell@gmail.com>
2020-08-31 18:12:52 +0100

    3 files modified

--- src/app/hier/hier.component.ts
+++ src/app/hier/hier.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { OnsNavigator } from 'ngx-onsenui';
 import { Globals } from '../globals';
  
@@ -14,29 +14,50 @@ export class HierComponent implements OnInit {
   hier = [];
    hier2 = [];
   hier_array = [];
-
+  max_c = 0;
+  @ViewChild('carousel_hier') carousel;
   hier_icons= '[{"level":"Organisation","icon":"building"},{"level":"Country","icon":"globe"},{"level":"Region","icon":"compass"},{"level":"County / Area / Scottish Region / Overseas Branch","icon":"map-marker-alt"},{"level":"District","icon":"map-marked-alt"},{"level":"Group","icon":"users"},{"level":"Section","icon":"user-friends"} ]'
 
 constructor(private navi: OnsNavigator,
               private globals: Globals,) {
   } 
 
-get_icon(d) {
-  if (d.indexOf('Region')==0) {d='Region'} 
-  var i = this.hier_array.find(a => (a.level == d));
- if (i==undefined) { return ""} 
-  return "fa_"+i.icon;
-}
+ g2(i) {
+  if (i>=0) {
+   this.carousel.nativeElement.setActiveIndex(i);
+  }
+  if (i==-1) {
+   this.carousel.nativeElement.prev();
+  }
+   if (i==-2) {
+   this.carousel.nativeElement.next();
+  }
+  }
 is_hier(s,t){
  // debugger;
   if (t!="--- Not Selected ---"&&t!="--- No Items Available ---") { 
   return s.substring(0,31)=='ctl00_workarea_cbo_p1_location_' } else return false
 }
   ngOnInit() {
+     this.max_c = (window.innerWidth - 75) / 40; 
     this.displayuser=this.globals.compassuser[0];
     this.hier=[]
     this.hier_array = JSON.parse(this.hier_icons)
      
+document.querySelector('#carousel_hier').addEventListener('postchange', function() {
+     if(document.querySelectorAll('.indicators').length>0 ){
+     for (var k=0;k<event.carousel.itemCount; k++){ 
+     document.querySelectorAll('.indicators')[k].innerHTML = '○';
+     }
+     document.querySelectorAll('.indicators')[event.activeIndex].innerHTML = '●';
+     }
+     var elmnt = document.querySelectorAll('.training2')[0].scrollTo({
+  top: 0,  left: 0,  behavior: 'auto' })
+      
+    
+       
+   })
+
   }
   
 }
